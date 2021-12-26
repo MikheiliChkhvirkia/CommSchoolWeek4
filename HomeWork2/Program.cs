5c49595783424b8e517e6edc5fb6ae441112db90
MikheilCh <91428850+MikheilCh@users.noreply.github.com>
2021-12-26 15:36:06 +0400

    Initial commit

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HomeWork2
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo input;
+            string name;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"My name is {Identity().Name} {Identity().Surname}");
+            do
+            {
+                Console.WriteLine("What is your Name (Name and Surname) ? : ");
+                name = Console.ReadLine();
+                InputParseAndSave(name);
+                if (InputParseAndSave(name).Name != null)
+                {
+                    Console.WriteLine($"Your Name is {InputParseAndSave(name).Name} {InputParseAndSave(name).Surname} ");
+                }
+                else
+                {
+                    Console.WriteLine($"Your Name Or Surname is Written Incorrectly : {name}\nIf you wish to Try again press Any Key \nTo Exit Press ESC");
+                }
+                input = Console.ReadKey();
+            }while(input.Key != ConsoleKey.Escape);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static identity InputParseAndSave(string name)
+        {
+            if (name.Contains(' '))
+            {
+                string[] nameAndSurname = name.Split(' ');
+                return new identity()
+                {
+                    Name = nameAndSurname[0],
+                    Surname = nameAndSurname[1]
+                };
+            }
+            else
+            {
+                return new identity()
+                {
+                    Name = null,
+                    Surname = null
+                };
+            }
+        }
+        public static identity Identity()
+        {
+            return new identity()
+            {
+                Name = "Mikheil",
+                Surname = "Chkhvirkia"
+            };
+        }
+    }
+}
---
+++ HomeWork2/identity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork2
+{
+    public class identity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
